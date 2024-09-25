cd59576a2d7809033bea699659acb3edd507b8ae
Himanshu Kumar <himanshukumar802123@gmail.com>
2024-09-25 17:07:17 +0530

    all three section ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './components/Navbar'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Listing from './components/Listing'
-import db from '../db.json'
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Listing from "./components/Listing";
+import db from "../db.json";
+import Image from "../Image.json";
 const App = () => {
-const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [image, setImage] = useState([]);
 
-useEffect(() =>{
+  useEffect(() => {
     setData(db);
-},[])
+  }, []);
+
+  useEffect(() => {
+    setImage(Image);
+  }, []);
 
   return (
     <>
-      <Navbar/>
-      <Header/>
-      <Hero/>
-      <Listing 
-      title = "We've got what everyone's listening to"
-      description = "Best sellers. New releases. That story you've been waiting for."
-      data = {data}
+      <Navbar />
+      <Header />
+      <Hero />
+      <Listing
+        title="We've got what everyone's listening to"
+        description="Best sellers. New releases. That story you've been waiting for."
+        data={data}
+      />
+
+      <Listing
+        title="Only from Audible"
+        description="Groundbreaking Originals and exclusives from A-list celebs and emerging talent."
+        data={image}
+      />
+
+      <Listing
+        title="The listening never has to stop"
+        description="Thousands of included titles in the Plus Catalogue. No cap on listening time."
+        data={data}
       />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Listing = ({title, description, data}) => {
-    console.log(data)
+const Listing = ({ title, description, data }) => {
+  console.log(data);
   return (
-    <div>
-        <div className='album-listing'>
-            <h1>
-                {title}
-            </h1>
-            <p>{description}</p>
-        </div>
-        <div className='head-booklisting invisible-scrollbar'>
-           { data.map((item) => (
-                <div key={item.id}>
-                    <img src={item.image} alt="" />
-                    <p style={{fontWeight:'600'}}>{item.name}</p>
-                    <p style={{color:'gray'}}>Written By: <span>{item.writtenBy}</span></p>
-                </div>
-            ))}
-        </div>
-      
+    <div className="album-top">
+      <div className="album-listing">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+      <div className="head-booklisting invisible-scrollbar">
+        {data.map((item) => (
+          <div key={item.id}>
+            <img src={item.image} alt="" />
+            {item.name && <p style={{ fontWeight: "600" }}>{item.name}</p>}
+            {item.writtenBy && <p style={{ color: "gray" }}>
+              Written By: <span>{item.writtenBy}</span>
+            </p>}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Listing;
